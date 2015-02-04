66dcc7373973ba0727552f7a011e5bc68e5fb8fb
TheVolatileV <elijahnhursey@gmail.com>
2015-02-04 17:01:01 -0500

    Create Player.java
    Skeleton code for Player class

---
+++ Player.java
@@ -0,0 +1,98 @@
+import java.awt.Color;
+
+
+public class Player implements Entity
+{
+    int health;
+    int AD;
+    int AP;
+    int armor;
+    int MR;
+    Color c;
+    int mana;
+
+    @Override
+    public int getHealth()
+    {
+        return 0;
+    }
+
+    @Override
+    public void setHealth(int health)
+    { 
+        
+    }
+
+    @Override
+    public int getAD()
+    {         
+        return 0;
+    }
+
+    @Override
+    public void setAD(int AD)
+    {         
+        
+    }
+
+    @Override
+    public int getAP()
+    {         
+        return 0;
+    }
+
+    @Override
+    public void setAP(int AP)
+    {         
+        
+    }
+
+    @Override
+    public int getArmor()
+    {        
+        return 0;
+    }
+
+    @Override
+    public void setArmor(int armor)
+    {    
+        
+    }
+
+    @Override
+    public int getMR()
+    {    
+        return 0;
+    }
+
+    @Override
+    public void setMR(int MR)
+    {    
+        
+    }
+
+    @Override
+    public int getColor()
+    {    
+        return 0;
+    }
+
+    @Override
+    public void setColor(Color c)
+    {    
+        
+    }
+
+    @Override
+    public int getMana()
+    {    
+        return 0;
+    }
+
+    @Override
+    public void setMana(int mana)
+    {    
+        
+    }
+
+}
